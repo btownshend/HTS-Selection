7935a33fb7a243be038598f778add16a5d93a99d
Brent Townshend <bst@tc.com>
2020-01-29 22:26:12 -0800

    Remove debug messages from piecewise()

--- src/lib/piecewise.m
+++ src/lib/piecewise.m
@@ -7,7 +7,7 @@ y=y(ia);
 sel=isfinite(x)&isfinite(y);
 x=x(sel); y=y(sel);
 [fit,stats]=robustfit(x,y);
-fprintf('Piecewise over %d points [%f-%f] -> %f\n', length(x), min(x), max(x),stats.robust_s);
+%fprintf('Piecewise over %d points [%f-%f] -> %f\n', length(x), min(x), max(x),stats.robust_s);
 assert(fit(2)>0); % Make sure it is monotonic increasing
 if stats.robust_s<maxrmse || length(x)<minpoints*2 || maxsegments<2
   map=[min(x),fit(1)+fit(2)*min(x); max(x), fit(1)+fit(2)*max(x)];
@@ -25,11 +25,11 @@ for i=minpoints:length(x)-minpoints
 end
 [~,i]=min(stotal);
 i=i+minpoints-1;
-fprintf('Using i=%d\n', i);
+%fprintf('Using i=%d\n', i);
 [map1,s1]=piecewise(x(1:i),y(1:i),maxrmse,ceil(maxsegments/2));
 [map2,s2]=piecewise(x(i+1:end),y(i+1:end),maxrmse,floor(maxsegments/2));
 sigma=sqrt((s1^2*i + (length(x)-i)*s2^2)/length(x));
 map=[map1(1:end-1,:);(map1(end,:)+map2(1,:))/2;map2(2:end,:)];
-fprintf('Split x(1:%d) at %d -> %f, %f -> %f\n', length(x), i, s1, s2, sigma);
+%fprintf('Split x(1:%d) at %d -> %f, %f -> %f\n', length(x), i, s1, s2, sigma);
 end
 
