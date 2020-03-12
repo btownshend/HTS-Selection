bb159252bbe1f1ebf98c7ae42d39877a9c488da9
Brent Townshend <bst@tc.com>
2020-03-11 18:49:31 -0700

    Updated maps

--- src/scripts/setupcompounds.m
+++ src/scripts/setupcompounds.m
@@ -39,23 +39,23 @@ for i=1:length(allfiles)
                   2591 2380];
   elseif ~isempty(strfind(allfiles(i).folder,'20200225'))
     maps(i).mz=[133,133+3e-4;521,521+19e-4];
-    maps(i).time=[304 302
-                  2591 2580];
+    maps(i).time=[203 202
+                  2894 2880];
   elseif ~isempty(strfind(allfiles(i).folder,'20200310'))
-    maps(i).mz=[133,133+3e-4;521,521+19e-4];
-    maps(i).time=[304 302
-                  2591 2580];
+    maps(i).mz=[133,133-4e-4;521,521+5e-4];
+    maps(i).time=[203 211
+                  2894 2891];
   elseif ~isempty(strfind(allfiles(i).folder,'20190309'))
     maps(i).mz=[133,133-3e-4;521,521+21e-4];
-    maps(i).time=[304 280
-                  2591 2578];
+    maps(i).time=[203 179
+                  2894 2883];
   elseif ~isempty(strfind(allfiles(i).folder,'20190427'))
     maps(i).mz=[133,133-24e-4;521,521-65e-4];
     maps(i).time=[0 0; 1 1 ];
   elseif ~isempty(strfind(allfiles(i).folder,'20190501'))
     maps(i).mz=[133,133-1e-4;521,521+7e-4];
-    maps(i).time=[304 284
-                  2560 2551];
+    maps(i).time=[204 185
+                  2894 2884];
   else
     error('No mapping for folder %s',allfiles(i).folder);
   end
