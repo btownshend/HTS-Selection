3390ec6f759f2895a54d65831c25f658647017eb
Brent Townshend <bst@tc.com>
2020-03-12 10:55:46 -0700

    Updated offset maps

--- src/scripts/setupcompounds.m
+++ src/scripts/setupcompounds.m
@@ -38,22 +38,22 @@ for i=1:length(allfiles)
                   2015 1700
                   2591 2380];
   elseif ~isempty(strfind(allfiles(i).folder,'20200225'))
-    maps(i).mz=[133,133+3e-4;521,521+19e-4];
+    maps(i).mz=[133,133+3e-4;521,521+20e-4];
     maps(i).time=[203 202
                   2894 2880];
   elseif ~isempty(strfind(allfiles(i).folder,'20200310'))
-    maps(i).mz=[133,133-4e-4;521,521+5e-4];
+    maps(i).mz=[133,133-4e-8;521,521-3e-4];
     maps(i).time=[203 211
                   2894 2891];
   elseif ~isempty(strfind(allfiles(i).folder,'20190309'))
-    maps(i).mz=[133,133-3e-4;521,521+21e-4];
+    maps(i).mz=[133,133-3e-4;521,521+20e-4];
     maps(i).time=[203 179
                   2894 2883];
   elseif ~isempty(strfind(allfiles(i).folder,'20190427'))
-    maps(i).mz=[133,133-24e-4;521,521-65e-4];
+    maps(i).mz=[133,133-24e-4;521,521-64e-4];
     maps(i).time=[0 0; 1 1 ];
   elseif ~isempty(strfind(allfiles(i).folder,'20190501'))
-    maps(i).mz=[133,133-1e-4;521,521+7e-4];
+    maps(i).mz=[133,133+1e-4;521,521+6e-4];
     maps(i).time=[204 185
                   2894 2884];
   else
