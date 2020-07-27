719a72b81e2acadfbde81504597fecc05e7b4277
Brent Townshend <bst@tc.com>
2020-07-27 16:50:58 -0700

    Handling mismatched ordering of vecs and compounds

--- matlab/S7/s7contains.m
+++ matlab/S7/s7contains.m
@@ -6,6 +6,12 @@ function c=s7contains(compounds, vecs, name)
   elseif strcmp(name,'V5120')
     c(:)=true;
   else
+    % Line up vectors with compounds 
+    vplate96=vecs.csvdata.srcplate96;
+    vwell96=vecs.csvdata.srcwell96;
+    ord=cellfun(@(z) find(strcmp(z.BATCH_PLATE,vplate96) & strcmp(z.BATCH_WELL,vwell96)), compounds.sdf);
+    assert(length(vplate96)==length(ord));
+    
     n=strsplit(name,'-');
     assert(length(n)==2);
     row=n{2}(1)-'A'+1;
@@ -16,29 +22,30 @@ function c=s7contains(compounds, vecs, name)
     end
     if strcmp(n{1},'V256A')
       index=(col-1)*8+row;
-      c=vecs.v256(index,:);
+      c=vecs.v256(index,ord);
       assert(sum(c)==256);
     elseif strcmp(n{1},'V256B')
       index=(col-1)*8+row+96;
-      c=vecs.v256(index,:);
+      c=vecs.v256(index,ord);
       assert(sum(c)==256);
     elseif strcmp(n{1},'V64A')
       index=(col-1)*16+row;
-      c=vecs.v64(index,:);
+      c=vecs.v64(index,ord);
       assert(sum(c)==64);
     elseif strcmp(n{1},'V64B')
       index=(col-1)*16+row+384;
-      c=vecs.v64(index,:);
+      c=vecs.v64(index,ord);
       assert(sum(c)==64);
     elseif strcmp(n{1},'PR80')
       assert(mod(col,3)~=0);
       group=floor((col-1)/3)+1;
+      plates=arrayfun(@(z) sprintf('CDIQ%04d',z),(group-1)*160+5:40:group*160+5-1,'Unif',false);
       index=(col-1-(group-1)*3)*8+row;
-      c((group-1)*320*4+index:16:group*320*4)=true;
+      c=cellfun(@(z) z.Well384(1)-'A'+1==index & ismember(z.Plate384,plates),compounds.sdf);
       assert(sum(c)==80);
     elseif strcmp(n{1},'R320')
       index=row;
-      c(index:16:end)=true;
+      c=cellfun(@(z) z.Well384(1)-'A'+1==row,compounds.sdf);
       assert(sum(c)==320);
     elseif strncmp(n{1},'CDIQ',4)
       plate=str2num(n{1}(5:end));
