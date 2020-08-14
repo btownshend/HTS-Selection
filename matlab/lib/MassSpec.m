d256ca4b3908174bfd4274674e67feea15f5b288
Brent Townshend <bst@tc.com>
2020-08-14 16:14:22 -0700

    Removed obsolete ident data/code from MassSpec
    Replaced by featurelists

--- matlab/lib/MassSpec.m
+++ matlab/lib/MassSpec.m
@@ -7,7 +7,6 @@ classdef MassSpec < handle
     time;   % Time of elutions (in minutes)
     mzrange;   % Range [low,high] of m/z to plot/consider
     resamp;   % struct (mz,y,n) of uniformly sampled data
-    ident;    % struct array of identified peaks
     featurelists;   % Array of feature lists
     mzxml;
   end
@@ -313,52 +312,6 @@ classdef MassSpec < handle
       end
     end
 
-    function listidents(obj)
-    %  List all the identified compounds
-      for i=1:length(obj.ident)
-        id=obj.ident(i);
-        fprintf('%2d %-12.12s M/Z=%8.4f\n',i, id.name,id.mztarget);
-        for k=1:length(id.peaks)
-          pk=id.peaks(k);
-          fprintf('   %12.12s     %8.4f  T=[%6.2f,%6.2f,%6.2f]  IC=%8.0f\n',  '',pk.mzobs, pk.elutions, pk.ictotal);
-        end
-      end
-    end
-
-    function plotident(obj,i)
-    % Make plot for i-th identification entry
-      id=obj.ident(i);
-      if isempty(id.peaks)
-        fprintf('No peaks for ident(%d)\n',i);
-        return;
-      end
-      
-      setfig(sprintf(' %.4f',id.mztarget));clf;
-
-      subplot(211);
-      pk=id.peaks(1);
-      semilogy(obj.time,id.ioncount);
-      hold on;
-      ax=axis;
-      tmargin=ceil((pk.elutions(3)-pk.elutions(1))/4);
-      ax(1)=obj.time(max(1,pk.elutions(1)-tmargin));
-      ax(2)=obj.time(min(length(obj.peaks),pk.elutions(3)+tmargin));
-      axis(ax);
-      plot(ax(1:2),pk.icthresh*[1,1],':r');
-      plot(obj.time(pk.elutions(1))*[1,1],ax(3:4),':r');
-      plot(obj.time(pk.elutions(2))*[1,1],ax(3:4),':g');
-      plot(obj.time(pk.elutions(3))*[1,1],ax(3:4),':r');
-      xlabel('Time (min)');
-      ylabel('Ion count');
-      title(sprintf('M/Z %.4f (err=%.4f) IC=%.0f',id.mztarget,pk.mzobs-id.mztarget,pk.ictotal));
-
-      subplot(212);
-      stem(obj.peaks{pk.elutions(2)}(:,1),obj.peaks{pk.elutions(2)}(:,2));
-      xlabel('m/z');
-      ylabel('Ion count');
-      title(sprintf('T(%d)=%f',pk.elutions(2),obj.time(pk.elutions(2))));
-    end
-
     function buildchromatograms(obj,varargin)
     % Build chromatograms using ADAP algorithm (as documented in mzmine2 help)
     % We define ε to be the mass tolerance parameter m/z.
@@ -511,10 +464,6 @@ classdef MassSpec < handle
       ylabel('Relative Intensity')
       hold on;
       ax=axis;
-      for i=1:length(obj.ident)
-        id=obj.ident(i);
-        plot(id.mztarget*[1,1],[0,ax(4)*1.1],':r')
-      end
       % List major peaks
       [~,ord]=sort(pks(:,2),'desc');
       for ii=1:args.maxlabels
@@ -544,18 +493,6 @@ classdef MassSpec < handle
       ylabel('Relative Intensity')
       hold on;
       ax=axis;
-      for i=1:length(obj.ident)
-        id=obj.ident(i);
-        for k=1:length(id.peaks)
-          pk=id.peaks(k);
-          if k==1
-            col='g';  % Green for main peak
-          else
-            col='r';   % Red for others
-          end
-          plot(obj.time(pk.elutions(2))*[1,1],[0,ax(4)*1.1],[':',col])
-        end
-      end
     end
 
     function map=timealign(obj,o2,step,window)
@@ -577,90 +514,6 @@ classdef MassSpec < handle
       plot(map(:,1),map(:,2)-map(:,1),'.-');
       xlabel(obj.name);
       ylabel(o2.name);
-    end
-    
-    
-    function x=compare(obj,o2)
-    % Compare identified peaks with another MassSpec object
-      [c,ia,ib]=intersect({obj.ident.name},{o2.ident.name});
-
-      fprintf('Matched %d of %d+%d identified compounds\n', length(c), length(obj.ident), length(o2.ident));
-      fprintf('                         %20.20s                      %20.20s\n', obj.name, o2.name);
-      %fprintf('Name           M/Z Exp.   dM/Z Elution Ion Cnt        dM/Z Elution Ion Cnt  IC 2/1\n');
-      elutions=[];
-      x=table();
-      for i=1:length(c)
-        i1=obj.ident(ia(i));
-        i2=o2.ident(ib(i));
-        if i1.mztarget~=i2.mztarget
-          fprintf('Warning: %s has different M/Z targets (%.3f, %.3f)\n', i1.mztarget, i2.mztarget);
-        end
-
-        peakdelta=1e10;
-        pk1=1;pk2=1;
-        for k1=1:length(i1.peaks)
-          t1=obj.time(i1.peaks(k1).elutions(2));
-          for k2=1:length(i2.peaks)
-            t2=o2.time(i2.peaks(k2).elutions(2));
-            if abs(t1-t2)<abs(peakdelta)
-              pk1=k1;
-              pk2=k2;
-              peakdelta=t2-t1;
-            end
-          end
-        end
-        if peakdelta==1e10
-          continue;
-        end
-        p1=i1.peaks(pk1);
-        p2=i2.peaks(pk2);
-        row=table();
-        row.name=i1.name;
-        row.mztarget=i1.mztarget;
-        row.id1=sprintf('%4d.%-3d',ia(i),pk1);
-        row.mzdelta1=round(p1.mzobs-i1.mztarget,3);
-        row.elution1=obj.time(p1.elutions(2));
-        row.ic1=round(p1.ictotal,0);
-
-        row.id2=sprintf('%4d.%-2d',ib(i),pk2);
-        row.mzdelta2=round(p2.mzobs-i1.mztarget,3);
-        row.elution2=o2.time(p2.elutions(2));
-        row.ic2=round(p2.ictotal,0);
-        row.tdelta=round(peakdelta,2);
-        row.flags='  ';
-        if abs(p1.mzobs-p2.mzobs)>0.005
-          row.flags(1)='M';
-        end
-        if abs(peakdelta)>10
-          row.flags(2)='T';
-        end
-        %fprintf('%-12.12s   %7.3f  %6.3f %7.2f %7.0f      %6.3f %7.2f %7.0f %6.1f%%\n', c{i}, i1.mztarget, p1.mzobs-i1.mztarget, p1.elutions(2), p1.ictotal, i2.mzobs-i1.mztarget, p2.elutions(2), p2.ictotal, p2.ictotal/p1.ictotal*scaling*100);
-        x=vertcat(x,row);
-      end
-      scaling=median(x.ic1./x.ic2);   % Scale factor to match ion counts
-      x.icratio=round(100*x.ic2./x.ic1*scaling,1);
-      disp(x)
-      ti=sprintf('%s vs %s',obj.name, o2.name);
-
-      setfig(ti);clf;
-      subplot(221);
-      loglog(x.ic1, x.ic2,'o');
-      xlabel(sprintf('Ion Count %s',obj.name));
-      ylabel(sprintf('Ion Count %s',o2.name));
-      ax=axis;
-      hold on;
-      plot(ax(1:2),ax(1:2)/scaling,':r');
-      title(ti);
-      subplot(222);
-      plot(x.mztarget, x.mzdelta1,'o');
-      hold on;
-      plot(x.mztarget, x.mzdelta2,'x');
-      xlabel('M/Z Exact')
-      ylabel('M/Z Diff')
-      subplot(223);
-      plot(x.elution1,x.elution2,'o');
-      xlabel('T1 (sec)');
-      ylabel('T2 (sec)')
     end
         
   end % methods
