1615cc4520cfa63c720bbd706f7ecff7b048a4f7
Brent Townshend <bst@tc.com>
2020-08-13 22:41:13 -0700

    Changed name of dir variable

--- matlab/scripts/savemz.m
+++ matlab/scripts/savemz.m
@@ -1,9 +1,9 @@
-dir='/Users/bst/TMP/mzdata';
-fprintf('Saving mzdata in %s...',dir);
+mzdir='/Users/bst/TMP/mzdata';
+fprintf('Saving mzdata in %s...',mzdir);
 for i=1:length(mzdata)
   fprintf('%d...',i);
   mzd=mzdata{i};
-  fname=sprintf('%s/%d.mat',dir,i);
+  fname=sprintf('%s/%d.mat',mzdir,i);
   save(fname,'mzd');
 end
 fprintf('done\n');
