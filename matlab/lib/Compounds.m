130cca069011a9907d0d8fb20d465468e2e30f20
Brent Townshend <bst@tc.com>
2020-08-13 22:39:05 -0700

    Use closest feature rather than highest feature when ambiguous

--- matlab/lib/Compounds.m
+++ matlab/lib/Compounds.m
@@ -530,7 +530,7 @@ classdef Compounds < handle
                     if args.debug
                       fprintf('Have %d ambiguous features for %s[%s] in %s\n', length(fi), obj.names{i},obj.ADDUCTS(k).name,obj.samples{j});
                     end
-                    [~,mind]=max([obj.reffeatures(j).features(fi).intensity]);
+                    [~,mind]=min(abs([obj.reffeatures(j).features(fi).time]-obj.meantime(i)));
                     fi=fi(mind);
                   end
                   obj.featureindex(i,kk,j)=fi;
