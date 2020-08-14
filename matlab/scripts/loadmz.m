42e16bd78d580b6df1e882380e2b344409ee9faa
Brent Townshend <bst@tc.com>
2020-08-13 22:41:26 -0700

    Added loadmz

---
+++ matlab/scripts/loadmz.m
@@ -0,0 +1,10 @@
+mzdir='/Users/bst/TMP/mzdata';
+fprintf('Loading mzdata in %s...',mzdir);
+for i=1:60
+  fprintf('%d...',i);
+  fname=sprintf('%s/%d.mat',mzdir,i);
+  mzd=load(fname);
+  mzdata{i}=mzd.mzd;
+end
+fprintf('done\n');
+clear mzd;
