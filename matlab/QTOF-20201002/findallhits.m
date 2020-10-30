166dce5f777a67f8e3badc68f08ae955198e43ab
Brent Townshend <bst@tc.com>
2020-10-29 22:54:22 -0700

    Added more adducts, diary

--- matlab/QTOF-20201002/findallhits.m
+++ matlab/QTOF-20201002/findallhits.m
@@ -1,5 +1,8 @@
-adducts=struct('name',{'M+H','M+Na','M+K'},'mass',{1.007276,22.989218,38.963158});
+adducts=struct('name',{'M+H','M+Na','M+K','M+NH4','M+DMSO+H'},...
+               'mass',{1.007276,22.989218,38.963158,18.033823,79.02122});  % From https://fiehnlab.ucdavis.edu/staff/kind/metabolomics/ms-adduct-calculator/
 
+debug=true;
+diary findallhits.txt
 fd=fopen('ident.csv','a');
 fprintf(fd,'source,compound,cname,adduct,m/z,time,intensity,nisotopes\n');
 bestfeat=Feature.empty;bestmz=[];
@@ -9,7 +12,7 @@ for i=1:size(qsetup.contains,2)
     continue;
   end
   for j=1:length(cont)
-    [bestfeature,bestmz,nisotopes]=mzdata{i}.findtarget(qsetup.sdf.sdf(cont(j)).Formula,'adducts',adducts,'mztol',0.002,'noise',500,'timetol',.6,'dbsave',false);
+    [bestfeature,bestmz,nisotopes]=mzdata{i}.findtarget(qsetup.sdf.sdf(cont(j)).Formula,'adducts',adducts,'mztol',0.002,'noise',500,'timetol',.6,'dbsave',false,'debug',debug);
     fprintf(fd,'%s,%d,"%s"',mzdata{i}.name,cont(j),qsetup.names{cont(j)});
     if isfinite(bestmz)
       fprintf(fd,',%s,%.5f,%.2f,%.0f,%d\n',bestfeature.name,bestmz,bestfeature.time,bestfeature.intensity,nisotopes);
@@ -18,3 +21,4 @@ for i=1:size(qsetup.contains,2)
     end
   end
 end
+diary off
