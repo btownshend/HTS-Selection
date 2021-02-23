3246daf320fa67d329257e791df113b616ffa1ca
Brent Townshend <bst@tc.com>
2021-02-22 21:29:06 -0800

    Load only some wells of interest

--- python/moldist/src/data/make_dataset.py
+++ python/moldist/src/data/make_dataset.py
@@ -8,10 +8,12 @@ def load():
     suppl = AllChem.SDMolSupplier(os.getenv("DATA") + "/HTBCFiles/ChemDivFull.sdf")
 
     # Extract only the 960 tested molecules
-    plates = ["CDIV%04d" % p for p in range(1, 121, 10)]
+    # plates = ["CDIV%04d" % p for p in range(1, 121, 10)]
     # print(len(plates), plates)
-    tested = [x for x in suppl if x.GetProp("BATCH_PLATE") in plates]
-    # print(len(suppl), len(tested))
+    wells=["045B02","206E05","208H04","247B08","247C06","247E05","286E11","287E04","326C10","445F10","446A08","446G07","446G08","447E02","448H09","485D02","486E07","487E04","487F04","488D10","525D02","526A09","526B09","526D09","526H07","526H11","568B08","568C04","568C07","607C11"]
+    # tested = [x for x in suppl if x.GetProp("BATCH_PLATE") in plates]
+    tested = [x for x in suppl if x.GetProp("BATCH_PLATE")[-3:]+x.GetProp("BATCH_WELL") in wells]
+    print(len(suppl), len(tested))
 
     # Set NAME property of molecules to match data from Matlab
     for mol in tested:
@@ -30,9 +32,10 @@ def load():
     return tested
 
 def dumpInchi(molecules):
-    for mol in molecules[:5]:
+    for mol in molecules:
+        AllChem.AddHs(mol)  # Not sure if this is needed, but seems idempotent
         inchi=AllChem.MolToInchi(mol)
         smiles=AllChem.MolToSmiles(mol)
         print(f"{mol.GetProp('NAME')}\n{inchi}\n{smiles}\n\n")
-        c=pc.get_compounds(inchi,'inchi')
-        print(c)
+        #c=pc.get_compounds(inchi,'inchi')
+        #print(c)
